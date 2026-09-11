43785c0c7789cbc004f0b5c102b9c9fa672502ac
Ali Goodarzi <alialigoodarzi@gmail.com>
2026-09-11 10:03:59 +0330

    feat: configure ef core db context

---
+++ BlogSystem.Api/Data/BlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BlogSystem.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogSystem.Api.Data;
+
+public class BlogDbContext : DbContext
+{
+    public BlogDbContext(DbContextOptions<BlogDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Author> Authors { get; set; }
+
+    public DbSet<Post> Posts { get; set; }
+}
